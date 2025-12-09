b6cc22bdf3576b8376a737911930b1ab145d90c9
chriswoodsanim <cjwoods268@gmail.com>
2025-12-09 15:33:28 +0000

    Update Tech page

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ gsap.to('h3', { duration: 0.5, y: 0, ease: 'power1.inOut', delay: 1.78})
 
 gsap.from('.container', { duration: 0.5, y: 400, ease: 'power1', opacity: 0, delay: 2, stagger: 0.1})
 
+function open(evt, tab) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
